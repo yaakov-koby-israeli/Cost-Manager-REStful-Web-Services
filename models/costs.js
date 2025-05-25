c2839c79a59cfa6bc91d17bec4c9a2f2a8cc5ecc
koby <kobyisr1@gmail.com>
2025-05-25 11:47:19 +0300

    add date section and require

--- models/costs.js
+++ models/costs.js
@@ -3,19 +3,27 @@ const Schema = mongoose.Schema;
 
 const CostSchema = new Schema({
     description: {
-        type: String
+        type: String,
+        required: true
     },
     category: {
         type: String, // "food", "health", "housing", "sport", "education"
-        enum: ['food', 'health', 'housing', 'sport', 'education']
+        enum: ['food', 'health', 'housing', 'sport', 'education'],
+        required: true
     },
     userid: {
-        type: String
+        type: String,
+        required: true
     },
     sum: {
-        type: Number
+        type: Number,
+        required: true
+    },
+    date: {
+        type: Date,
+        default: Date.now // אם לא מגיע מהלקוח, יתווסף אוטומטית
     }
 });
-const Cost = mongoose.model('costs', CostSchema);
+const Cost = mongoose.model('costs', CostSchema); // מחזירה רפרנס לאובייקט שהוא בעצם קונסטרקטור פאנקשן
 
 module.exports = Cost;
