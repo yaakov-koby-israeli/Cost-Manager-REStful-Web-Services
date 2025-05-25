10a8815d9f3790f5b415c2b074e5968c70c377bd
koby <kobyisr1@gmail.com>
2025-05-25 11:14:41 +0300

    add require to id first name and last name

--- models/users.js
+++ models/users.js
@@ -2,14 +2,17 @@ const mongoose = require('mongoose');
 const Schema = mongoose.Schema;
 
 const UserSchema = new Schema({
-    id: {
-        type: String
+    _id: { // the _ before id means, this is the only id and mongoos will not create auto id.
+        type: String,
+        required: true
     },
     first_name: {
-        type: String
+        type: String,
+        required: true
     },
     last_name: {
-        type: String
+        type: String,
+        required: true
     },
     birthday: {
         type: Date
