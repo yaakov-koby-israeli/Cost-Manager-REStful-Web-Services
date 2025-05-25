7c818f5b8d404dea8fb1eadd3bdde922f080fdc6
koby <kobyisr1@gmail.com>
2025-05-25 14:54:25 +0300

    fix routes name

--- app.js
+++ app.js
@@ -10,7 +10,7 @@ const mongoose = require('mongoose');
 
 const indexRouter = require('./routes/index');
 const usersRouter = require('./routes/users');
-const costsRouter = require('./routes/costs');
+const apiRouter = require('./routes/api');
 
 const app = express();
 
@@ -29,7 +29,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/', indexRouter);
 app.use('/users', usersRouter);
-app.use('/costs', costsRouter);
+app.use('/api', apiRouter);
 
 // catch 404 and forward to error handler
 app.use(function(req, res, next) {
