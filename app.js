f8e8d96cd1e7af048aa4005b4b41a798c84a76a7
koby <kobyisr1@gmail.com>
2025-06-06 18:58:09 +0300

    clean app.js for REST API

--- app.js
+++ app.js
@@ -21,7 +21,6 @@ app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({ extended: false }));
 app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
 
 app.use('/api', apiRouter);
 
@@ -33,12 +32,9 @@ app.use(function(req, res, next) {
 // error handler
 app.use(function(err, req, res, next) {
   // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get('env') === 'development' ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render('error');
+  res.status(err.status || 500).json({
+    error: err.message
+  });
 });
 
 module.exports = app;
